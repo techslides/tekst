68b3c49c9b8a3560296be7e3666b95c5ea67b79b
kyledinh <kyledinh@gmail.com>
2013-08-13 15:40:50 -0700

    cleanup app.go

--- app.go
+++ app.go
@@ -52,7 +52,7 @@ func loadPage(title string) (*Page, error) {
 var templates = template.Must(template.ParseFiles("app.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-   err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+   err := templates.ExecuteTemplate(w, tmpl, p)
    if err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
@@ -74,32 +74,12 @@ func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.Hand
    }
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
    p, err := loadPage(title)
    if err != nil {
       p = &Page{Title: title}
    }
-   renderTemplate(w, "app", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-   body := r.FormValue("body") 
-   p := &Page{Title: title, Body: []byte(body)}
-   err := p.save()
-   if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-   }
-   http.Redirect(w, r, "/view/" + title, http.StatusFound)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-   p, err := loadPage(title)
-   if err != nil {
-      http.Redirect(w, r, "/edit/" + title, http.StatusFound)
-      return
-   }
-   renderTemplate(w, "app", p)
+   renderTemplate(w, "app.html", p)
 }
 
 func jsonRespond(w http.ResponseWriter, res Response) {
@@ -112,7 +92,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
    body, err := ioutil.ReadAll(r.Body)
    sw := tekst.Stopwatch {}
    if err != nil {
-      jsonRespond(w, Response{"status": "fail", "message": "Could not parse the Body from the Request."})
+      jsonRespond(w, Response{"status":"fail", "message":"Could not parse the Body from the Request."})
    }
    sw.Start()
    log.Println(string(body))
@@ -120,11 +100,11 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
    err = json.Unmarshal(body, &t)
    sw.Stop()
    if err != nil {
-      jsonRespond(w, Response{"status": "fail", "message": "Could not parse the JSON message."})
+      jsonRespond(w, Response{"status":"fail", "message":"Could not parse the JSON message."})
    }
    
    log.Println(t.Data + t.Message + t.Action)
-   jsonRespond(w, Response{"status": "success", "message": t.Message, "result": sw.Dur.String()})
+   jsonRespond(w, Response{"status":"success", "message": t.Message, "result": sw.Dur.String()})
    return
 }
 
@@ -134,7 +114,7 @@ func restHandler(w http.ResponseWriter, r *http.Request) {
    
    body, err := ioutil.ReadAll(r.Body)
    if err != nil {
-      jsonRespond(w, Response{"status": "fail", "message": "Could not parse the Body from the Request."})
+      jsonRespond(w, Response{"status":"fail", "message":"Could not parse the Body from the Request."})
    }
    
    log.Println(string(body))
@@ -143,7 +123,7 @@ func restHandler(w http.ResponseWriter, r *http.Request) {
    err = json.Unmarshal(body, &t)
    
    if err != nil {
-      jsonRespond(w, Response{"status": "fail", "message": "Could not parse the JSON message."})
+      jsonRespond(w, Response{"status":"fail", "message":"Could not parse the JSON message."})
    }
    
    arr := strings.Split(t.Data, " ")
@@ -154,16 +134,14 @@ func restHandler(w http.ResponseWriter, r *http.Request) {
    } 
    t.Message = sw.Stop()
    log.Println(t.Data + t.Message + t.Action)
-   jsonRespond(w, Response{"status": "success", "data": t.Result, "result": t.Message})
+   jsonRespond(w, Response{"status":"success", "data": t.Result, "result": t.Message})
    return
 }
 
 func main() {
-   http.HandleFunc("/edit/", makeHandler(editHandler))
-   http.HandleFunc("/save/", makeHandler(saveHandler))
-   http.HandleFunc("/view/", makeHandler(viewHandler))
    http.HandleFunc("/test", testHandler)
    http.HandleFunc("/rest", restHandler)
+   http.HandleFunc("/view/", makeHandler(viewHandler))   
    http.HandleFunc("/", makeHandler(viewHandler))
    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
    http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
