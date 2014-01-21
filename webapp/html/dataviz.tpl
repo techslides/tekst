b1dd75ce87810df3b2971b2265b9a9f26a12fb6c
kyledinh <kyledinh@gmail.com>
2014-01-21 10:25:43 -0800

    added donut-chart directive

---
+++ webapp/html/dataviz.tpl
@@ -0,0 +1,14 @@
+      <div><h1>Data Visualization with D3JS</h1>
+
+          <div class="col-lg-8" id="stage"> 
+              <h4>Donut Charts</h4>
+              <donut-chart data="shared.data" on-click="chartClicked()"></donut-chart>
+              <ul>
+                  <li ng-repeat="v in shared.data track by $index">
+                      <input type="range" min="0" max="100" ng-model="shared.data[$index]"></input>
+                  </li>
+                  <button ng-click="addValue()">Add value</button>
+              </ul>
+          </div><!-- col-lg-8 -->
+
+       </div><!-- DataDiz-->
--- webapp/js/app.js
+++ webapp/js/app.js
@@ -13,12 +13,13 @@ app.config(function ($routeProvider) {
         .when('/beta',  { controller:"BetaCtrl",  templateUrl:"/html/bootstrap.tpl" })      
         .when('/delta', { controller:"DeltaCtrl", templateUrl:"/html/mindsweep.tpl" })     
         .when('/gamma', { controller:"GammaCtrl", templateUrl:"/html/choropleth.tpl" })  
+        .when('/zeta', { controller:"ZetaCtrl", templateUrl:"/html/dataviz.tpl" })  
         .when('/error', { controller:"DeltaCtrl", templateUrl:"error_tpl", 
             resolve: {
                 makeError: deltaCtrl.makeError,
             } 
         }).otherwise( {template: "404 Not Found!"} );
-})
+});
 
 app.run(function ($rootScope, $log) {
     $rootScope.$log = $log;
@@ -27,14 +28,14 @@ app.run(function ($rootScope, $log) {
 // a service
 app.factory('DataService', function() {
     return { sourcetext: "The quick brown fox jumps over the lazy dog.", criteria: "WORD"};
-})
+});
 
 // a filter
 app.filter('reverse', function() {
     return function (text) {
         return text.split("").reverse().join("");
     }
-})
+});
 
 app.directive("error", function ($rootScope) {
     return {
@@ -46,13 +47,13 @@ app.directive("error", function ($rootScope) {
             })
         }
     }
-})
+});
 
 app.controller("AppCtrl", function ($rootScope) {  
     $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
         console.log(rejection);
     })    
-})
+});
 
 function AlphaCtrl($scope, $http, DataService) {
     $scope.data = DataService;
@@ -283,6 +284,111 @@ deltaCtrl.makeError = function ($q, $timeout) {
     }, 500);
     return defer.promise;
 };
+
+//<donut-chart>
+app.directive('donutChart', function() {
+    return {
+        scope: { 'data': '=', 'onClick': '&' },
+        restrict: 'E',
+        link: function(scope, element) {
+            var pie = new PieChart(element, scope.data);
+            pie.onClick = function() {
+                scope.$apply(function(){
+                    if(scope.onClick) scope.onClick();
+                });
+            }
+            scope.$watchCollection('data', pie.updateData);
+        }
+    };
+});
+
+app.controller('ZetaCtrl', function($scope){
+    // controller "knows" nothing about donut charts
+    $scope.shared = { data: [ 10, 20, 30, 40 ] };
+    $scope.chartClicked = function() {
+        var len = $scope.shared.data.length,
+            i;
+        for (i = 0; i < len; i++) { 
+            $scope.shared.data[i] = Math.floor(Math.random()*50)+1;
+            //removed this and underscore.js _.each($scope.shared.data, function(d, index) {
+        }
+    }
+    $scope.addValue = function() {
+        $scope.shared.data.push(Math.floor(Math.random()*30))+1;
+    }
+});
+
+/* 
+ * D3JS Charts 
+ */
+
+function PieChart(element, data) {
+    var self = this;
+    var color = d3.scale.category10();
+    var el = element[0];
+    var width = el.clientWidth;
+    var height = el.clientHeight;
+    var min = Math.min(width, height);
+    var pie = d3.layout.pie().sort(null);
+    var arc = d3.svg.arc()
+        .outerRadius(min / 2 * 0.9)
+        .innerRadius(min / 2 * 0.5);
+      
+    svg = d3.select(el).append('svg')
+        .attr({width: width, height: height})
+        .append('g')
+        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+    svg.on('mousedown', function(d) {
+        self.onClick();
+    });
+    
+    // add the <path>s for each arc slice
+    arcs = svg.selectAll('path.arc').data(pie(data))
+        .enter().append('path')
+        .attr('class', 'arc')
+        .style('stroke', 'white')
+        .attr('fill', function(d, i) { return color(i) })
+        // store the initial angles
+        .each(function(d) { return this._current = d });
+
+    var arcTween = function (a) {
+      // see: http://bl.ocks.org/mbostock/1346410
+        var i = d3.interpolate(this._current, a);
+        this._current = i(0);
+        return function(t) {
+            return arc(i(t));
+        };
+    }
+    this.onClick = function() {
+      
+    };
+    
+    this.updateData = function(newData, oldData) {
+        var data = newData.slice(0); // copy
+        var duration = 500;
+        var PI = Math.PI;
+        while(data.length < oldData.length) data.push(0);
+        arcs = svg.selectAll('.arc').data(pie(data));
+        arcs.transition().duration(duration).attrTween('d', arcTween);
+        // transition in any new slices
+        arcs.enter().append('path')
+            .style('stroke', 'white')
+            .attr('class', 'arc')
+            .attr('fill', function(d, i){ return color(i) })
+            .each(function(d) {
+                this._current = { startAngle: 2 * PI - 0.001, endAngle: 2 * PI }
+            })
+            .transition().duration(duration).attrTween('d', arcTween);
+        // transition out any slices with size = 0
+        arcs.filter(function(d){ return d.data === 0 })
+            .transition()
+            .duration(duration)
+            .each(function(d){ d.startAngle = 2 * PI - 0.001; d.endAngle = 2 * PI; })
+            .attrTween('d', arcTween).remove();
+    }
+}
+
 
 /* 
  * Bootstrap Utils
