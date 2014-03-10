fbb20065cd2a944a3d46c0fe98e7b5c35e3e2b0f
kyledinh <kyledinh@gmail.com>
2014-03-09 17:52:22 -0700

    refactored to fig1 obj

--- src/angular/ctrl_gamma.js
+++ src/angular/ctrl_gamma.js
@@ -1,22 +1,11 @@
 
 /* ctrl_gamma.js ------vv-------------- */
 function GammaCtrl($scope) {
-    var width = 960;
-    var height = 500;
-    var rateById = d3.map();
-    var quantize = d3.scale.quantize()
-        .domain([0, .15])
-        .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+    var fig1 = { id: "fig1", w: 960, h: 500, rateById: d3.map(), path: d3.geo.path(),
+                 quantize: null, svg: null };
 
-    var red_quantize = d3.scale.quantize()
-        .domain([0, .15])
-        .range(d3.range(9).map(function(i) { return "r" + i + "-9"; }));
-
-    var path = d3.geo.path();
-
-    var svg = d3.select("#stage").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+    fig1.svg = d3.select("#stage").append("svg")
+        .attr("width", fig1.w).attr("height", fig1.h);
 
     $scope.binh = [];
 
@@ -26,54 +15,61 @@ function GammaCtrl($scope) {
     };
 
     $scope.cold = function() {
-        svg.remove();
+        fig1.svg.remove();
         $scope.toggleSpinner();
-        //d3.select("svg").remove();
-        svg = d3.select("#stage").append("svg").attr("width", width).attr("height", height);
+
+        fig1.svg = d3.select("#stage").append("svg").attr("width", fig1.w).attr("height", fig1.h);
+        fig1.quantize = d3.scale.quantize().domain([0, .15])
+            .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
         queue()
         .defer(d3.json, "/data/us.json")
-        .defer(d3.tsv, "/data/cold.tsv", function(d) { rateById.set(d.id, +d.rate); })
+        .defer(d3.tsv, "/data/cold.tsv", function(d) { fig1.rateById.set(d.id, +d.rate); })
         .await(function(error, us) {
             $scope.toggleSpinner();
-            svg.append("g")
+            fig1.svg.append("g")
                 .attr("class", "counties")
                 .selectAll("path")
                 .data(topojson.feature(us, us.objects.counties).features)
                 .enter().append("path")
-                .attr("class", function(d) { return quantize(rateById.get(d.id)); })
-                .attr("d", path)
+                .attr("class", function(d) { return fig1.quantize(fig1.rateById.get(d.id)); })
+                .attr("d", fig1.path)
                 .on("click", $scope.clicked);
 
-            svg.append("path")
+            fig1.svg.append("path")
                 .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
                 .attr("class", "states")
-                .attr("d", path);
+                .attr("d", fig1.path);
         });
     };
 
     $scope.flu = function() {
-        svg.remove();
+
+        fig1.svg.remove();
         $scope.toggleSpinner();
-        //d3.select("svg").remove();
-        svg = d3.select("#stage").append("svg").attr("width", width).attr("height", height);
+
+        fig1.svg = d3.select("#stage").append("svg").attr("width", fig1.w).attr("height", fig1.h);
+        fig1.quantize = d3.scale.quantize().domain([0, .15])
+            .range(d3.range(9).map(function(i) { return "r" + i + "-9"; }));
+
         queue()
             .defer(d3.json, "/data/us.json")
-            .defer(d3.tsv, "/data/flu.tsv", function(d) { rateById.set(d.id, +d.rate); })
+            .defer(d3.tsv, "/data/flu.tsv", function(d) { fig1.rateById.set(d.id, +d.rate); })
             .await(function(error, us) {
                 $scope.toggleSpinner();
-                svg.append("g")
+                fig1.svg.append("g")
                     .attr("class", "counties")
                     .selectAll("path")
                     .data(topojson.feature(us, us.objects.counties).features)
                     .enter().append("path")
-                    .attr("class", function(d) { return red_quantize(rateById.get(d.id)); })
-                    .attr("d", path)
+                    .attr("class", function(d) { return fig1.quantize(fig1.rateById.get(d.id)); })
+                    .attr("d", fig1.path)
                     .on("click", $scope.clicked);
 
-                svg.append("path")
+                fig1.svg.append("path")
                     .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
                     .attr("class", "states")
-                    .attr("d", path);
+                    .attr("d", fig1.path);
             });
     };   
 
